22d6e5457b1c9acabb6db2090b02b586530221b5
Jinyao Ma <jinyao.ma@outlook.com>
2024-02-08 14:01:26 +0800

    add save view for vehicle

--- aerc_project/aerc_website/views.py
+++ aerc_project/aerc_website/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Vehicle, User, Asset, AssetType
+from enum import Enum, auto
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+
+class VIEWTYPE(Enum):
+    list = auto()
+    detail = auto()
 
 # Create your views here.
 
@@ -20,22 +26,44 @@ def index(request):
     else:
         return render(request, 'index.html')
 
+@csrf_exempt
 def vehicle(request):
     context = {}
     if request.method == "GET":
-        total = Vehicle.objects.count()
-        size = int(request.GET.get('size', 20))
-        page = int(request.GET.get('page', 1))
-        data = Vehicle.objects.all()[(page-1)*size:page*size]
-        context['total'] = total
-        context['size'] = size
-        context['page'] = page
-        context['data'] = data
-        context['hasPrev'] = page > 1
-        context['hasNext'] = page * size + len(data) != total
-        context['pagePrev'] = page - 1
-        context['pageNext'] = page + 1
-        return render(request, 'vehicle/index.html', context)
+        viewtype = request.GET.get('vt', 'list')
+        if VIEWTYPE[viewtype] is VIEWTYPE.list:
+            total = Vehicle.objects.count()
+            size = int(request.GET.get('size', 20))
+            page = int(request.GET.get('page', 1))
+            data = Vehicle.objects.all()[(page-1)*size:page*size]
+            context['total'] = total
+            context['size'] = size
+            context['page'] = page
+            context['data'] = data
+            context['hasPrev'] = page > 1
+            context['hasNext'] = page * size + len(data) != total
+            context['pagePrev'] = page - 1
+            context['pageNext'] = page + 1
+            return render(request, 'vehicle/index.html', context)
+        elif VIEWTYPE[viewtype] is VIEWTYPE.detail:
+            id = int(request.GET.get('id', 0))
+            context['id'] = id
+            if id > 0:
+                context['data'] = Vehicle.objects.get(id=id)
+            return render(request, 'vehicle/detail.html', context)
+    if request.method == "POST":
+        vehicleAsset = Asset.objects.get(category='V')
+        id = int(request.POST.get('id', 0))
+        brand = request.POST.get('brand', "")
+        model = request.POST.get('model', "")
+        year = request.POST.get('year', "")
+        color = request.POST.get('color', "")
+        VIN = request.POST.get('VIN', "")
+        purchase_price = request.POST.get('purchase_price', "")
+        purchase_date = request.POST.get('purchase_date', "")
+        a = Vehicle(asset_id=vehicleAsset.id, id=id, brand=brand, model=model, year=year, color=color, VIN=VIN, purchase_price=purchase_price, purchase_date=purchase_date)
+        a.save()
+        return redirect('vehicle')
 
 def user(request):
     context = {}
