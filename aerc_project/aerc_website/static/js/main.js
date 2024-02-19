2f890ef79c612875b72c25229842413456c8da6a
Silencepaul <xlymx0426@gmail.com>
2024-02-19 13:09:13 +0800

    stock verificatio update

--- aerc_project/aerc_website/static/js/main.js
+++ aerc_project/aerc_website/static/js/main.js
@@ -3,8 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
     stock_info = document.getElementById("stock_info");
 
     stock_verify.addEventListener("click", function() {
+        stock_verify.innerHTML = "Loading...";
+        stock_verify.disabled = true;
         stock_ticker = document.getElementById("stock_ticker");
         stock_ticker = stock_ticker.value;
-        htmx.ajax('GET', '/stock_search/' + stock_ticker, stock_info);
+        htmx.ajax('GET', '/stock_search/' + stock_ticker, stock_info).then(
+            function() {
+                stock_verify.innerHTML = "Verify";
+                stock_verify.disabled = false;
+            });
     });
 });
